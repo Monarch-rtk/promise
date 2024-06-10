7399b63ad9019c8a9f8ec8bbf03fdffee8215c97
Rainal Tennison <rainaltennison@Rainals-MacBook-Air.local>
2024-06-10 11:55:01 +0530

    Promise

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-j=0
-while(j<=20){
-    console.log(j);
-    j++
+function sum(n1,n2){
+    return new Promise(function(res,rej){
+        res(n1+n2)
+    })
 }
+
+function subsraction(n1,n2){
+    return new Promise((res, rej) => {
+        res(n1-n2)
+    })
+}
+function multiplication(n1,n2){
+    return new Promise((res,rej)=>{
+        res(n1*n2)
+    })
+}
+function division(n1,n2){
+    return new Promise((res,rej)=>{
+        res(n1/n2)
+    })
+}
+
+sum(12,20).then(function(data){
+    console.log(data);
+    return subsraction(data,18)
+}).then((data) => {
+    console.log(data);
+    return multiplication(data,8)
+}).then((data)=>{
+    console.log(data);
+    return division(data,2)
+}).then((data)=> {
+    console.log(data);
+})
